74467d4c6dc236829ca9a90cc4213b80b1697df3
ANJALI K P <93821243+AnjaliDithin@users.noreply.github.com>
2021-11-15 15:28:14 +0530

    New Task
    New task

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const myArr = [101,201,301,401,501];
-//using splice method returns removed elements,index is  0th postion and count is 5 //
-//Then slice method retutns selected elements in an array as new array,start position is 0 and end is 2// 
-//new array is arrTwo contain left elements only//
-const arrTwo = myArr.splice(0,5).slice(0,2);
-// 0 th position of new array is assigned new value//
-arrTwo[0] = "001";
-//print new array//
-console.log(arrTwo);
-
-
-
-//ouput is ['001',201]
+var x =[1,2,3,4,5];
+var y=[6,7,8,9,10];
+var z= x.concat(y);
+console.log(z);
+let arrayofArrays =[];
+function splitArray(array)
+{
+    while(array.length>0)
+       {
+        let arrayElement=array.splice(0,1);
+        arrayofArrays.push(arrayElement);
+        }
+        return arrayofArrays;
+}
+console.log(splitArray(z));
